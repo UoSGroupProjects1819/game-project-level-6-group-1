2d2109851f36d832f670c69a9e938a016ebd2190
Daniel Pokladek <danielpokladek@gmail.com>
2018-10-29 00:50:08 +0000

    Players can now input planet name, it won't save yet.

---
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] private InputField nameField;
+
+    private string planetName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSubmit()
+    {
+        planetName = nameField.text;
+
+        Debug.Log("Planet Name : " + planetName);
+    }
+}
